152be0c1d76d400f7947dac8e7690c9d004f5040
Artem <64410376+Artiss90@users.noreply.github.com>
2021-04-07 01:35:56 +0300

    fix PostItem

--- src/Components/PostsList/PostItem/PostItem.jsx
+++ src/Components/PostsList/PostItem/PostItem.jsx
@@ -1,16 +1,18 @@
 import { Link, withRouter } from 'react-router-dom';
 
 const PostItem = ({ title, body, id }) => {
-  <>
-    <Link
-      to={{
-        pathname: `/posts/${id}`,
-      }}
-    >
-      {title}
-    </Link>
-    <p>{body}</p>
-  </>;
+  return (
+    <>
+      <Link
+        to={{
+          pathname: `/posts/${id}`,
+        }}
+      >
+        {title}
+      </Link>
+      <p>{body}</p>
+    </>
+  );
 };
 
 export default withRouter(PostItem);
--- src/Components/PostsList/PostsLists.jsx
+++ src/Components/PostsList/PostsLists.jsx
@@ -1,17 +1,21 @@
 import { useSelector } from 'react-redux';
 import postSelectors from 'redux/postRedux/postSelectors';
 import style from './PostsLists.module.css';
+import PostsItems from './PostItem/PostItem';
 
 const PostsLists = () => {
   const posts = useSelector(postSelectors.getPostsItems);
 
   return (
     <ul className={style.list}>
-      {posts.map(post => (
-        <li key={post.id} className={style.item}>
-          <PostsLists id={post.id} title={post.title} body={post.body} />
-        </li>
-      ))}
+      {posts &&
+        posts.map(post => (
+          <li key={post.id} className={style.item}>
+            <PostsItems id={post.id} title={post.title} body={post.body} />
+            {/* <p>{post.title}</p>
+          <p>{post.body}</p> */}
+          </li>
+        ))}
     </ul>
   );
 };
